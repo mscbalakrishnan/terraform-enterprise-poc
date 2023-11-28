7564f36a1f7f645a6121758d7e74b738a339ff77
Bala Krishnan <mscbalakrishnan@gmail.com>
2023-11-28 14:47:35 +0530

    Removed one resource

--- main.tf
+++ main.tf
@@ -10,10 +10,7 @@ resource "aws_instance" "example1" {
   ami           = "ami-05d36ba3a628744e2"
   instance_type = "t2.micro"
 }
-resource "aws_instance" "example2" {
-  ami           = "ami-05d36ba3a628744e2"
-  instance_type = "t2.micro"
-}
+
 
 
 
