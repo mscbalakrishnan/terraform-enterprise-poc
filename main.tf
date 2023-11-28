62f7d5e3a19926eb63901b0769193fded9bd9876
Bala Krishnan <mscbalakrishnan@gmail.com>
2023-11-28 14:35:17 +0530

    test

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+
+resource "aws_instance" "example" {
+  ami           = "ami-05d36ba3a628744e2"
+  instance_type = "t2.micro"
+}
+
+
+resource "aws_instance" "example1" {
+  ami           = "ami-05d36ba3a628744e2"
+  instance_type = "t2.micro"
+}
+
+
+
---
+++ provider.tf
@@ -0,0 +1,27 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# The following variable is used to configure the provider's authentication
+# token. You don't need to provide a token on the command line to apply changes,
+# though: using the remote backend, Terraform will execute remotely in Terraform
+# Cloud where your token is already securely stored in your workspace!
+
+variable "provider_token" {
+  type = string
+  sensitive = true
+}
+
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
